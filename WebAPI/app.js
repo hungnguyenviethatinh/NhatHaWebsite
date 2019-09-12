c3241ad35df508ce00d43b2e58bdbec303c16dd4
Nguyen Viet Hung <hungnguyenviet.hatinh@gmail.com>
2019-09-12 19:18:11 +0700

    init code repos.

---
+++ WebAPI/app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const config = require("./config/database");
+
+mongoose.connect(
+  config.database,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+let db = mongoose.connection;
+
+// Check connection
+db.once("open", function() {
+  console.log("Connected to MongoDB");
+});
+
+// Check for DB errors
+db.on("error", function(err) {
+  console.log(err);
+});
+
+// Init App
+const app = express();
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Express Session Middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Express Messages Middleware
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+// Passport Config
+require("./config/passport")(passport);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("*", function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Home Route
+app.get("/", function(req, res) {
+  res.status(200).send('Hello!');
+});
+
+// Route Files
+let users = require("./routes/users");
+app.use("/users", users);
+
+// Start Server
+app.listen(3000, function() {
+  console.log("Server started on port 3000...");
+});
