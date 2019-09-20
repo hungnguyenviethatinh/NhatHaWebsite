ea82044c612a2331804b430191e0ec61d984ef0e
Nguyen Viet Hung <hungnguyenviet.hatinh@gmail.com>
2019-09-20 22:38:08 +0700

    show dropdown menu.

--- ClientApp/src/app/components/shared/header/header.component.ts
+++ ClientApp/src/app/components/shared/header/header.component.ts
@@ -2,29 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  isAuthenticated: boolean;
-  constructor() { 
-    this.isAuthenticated = false;
-  }
+    isAuthenticated: boolean;
+    constructor() {
+        this.isAuthenticated = false;
+    }
 
-  ngOnInit() {
-    this.showDropdown();
-  }
+    ngOnInit() {
+        this.bindShowDropdownMenu();
+    }
 
-  showDropdown = () => {
-    $('a[data-toggle="dropdown"]').hover(() => {
-      console.log('hover in');
-      $('div.dropdown-menu').addClass('show');
-    },
-    () => {
-      console.log('hover out');
-      $('div.dropdown-menu').removeClass('show');
-    });
-  }
+    bindShowDropdownMenu = () => {
+        $('#secondary-navbar li.nav-item > a').each( function(a) {
+            $(this).hover( () => {
+                $('#secondary-navbar li.nav-item > div.dropdown-menu').filter('.show').removeClass('show');
+                $(this).siblings('div.dropdown-menu').addClass('show');
+                $(this).siblings('div.dropdown-menu').hover( () => {
+                }, () => {
+                    $(this).siblings('div.dropdown-menu').removeClass('show');
+                });
+            }, () => {
+            });
+        });
+    }
 
 }
