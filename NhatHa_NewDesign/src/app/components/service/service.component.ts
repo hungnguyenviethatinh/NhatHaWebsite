61ef94f7a3ded6263ab11b61b3825585d5ad97a5
Nguyen Viet Hung <hungnguyenviet.hatinh@gmail.com>
2019-09-30 00:09:46 +0700

    [ui] service homepage. [i]

---
+++ NhatHa_NewDesign/src/app/components/service/service.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nh-service',
+  templateUrl: './service.component.html',
+  styleUrls: ['./service.component.less']
+})
+export class ServiceComponent implements OnInit {
+  banners = [
+    '/assets/images/services/banners/Montajes-Eletricos-Sevilla.JPG',
+    '/assets/images/services/banners/Electrical-Contractor.JPG',
+    '/assets/images/services/banners/Impianti-Elettrici.JPG',
+    '/assets/images/services/banners/Machine-goes-Ping.JPG',
+  ];
+
+  services = [
+    {
+      link: '/',
+      img: '/assets/images/services/6.JPG',
+      title: 'Phân Tích Hệ Thống Điện',
+      descriptions: [
+        'Tính Toán Ngắn Mạch – Phối Hợp Bảo Vệ',
+        'Phân Tích Nguy Cơ Hồ Quang Điện',
+      ],
+    },
+    {
+      link: '/',
+      img: '/assets/images/services/7.JPG',
+      title: 'Thử Nghiệm & Nghiệm Thu',
+      descriptions: [
+        'Thiết Bị Cao Thế, Tủ Trung Thế, Cáp Trung Thế',
+        'Máy Biến Áp Lực, Tủ Phân Phối Hạ Thế',
+      ],
+    },
+    {
+      link: '/',
+      img: '/assets/images/services/8.JPG',
+      title: 'Phân Tích Chất Lượng Điện',
+      descriptions: [
+        'Nhấp Nháy, Sóng Hài, Dòng Khởi Động',
+        'Mất Cân Bằng, Hệ Số Công Suất, Sụt Áp',
+      ],
+    },
+    {
+      link: '/',
+      img: '/assets/images/services/4.JPG',
+      title: 'Bảo Trì Ngăn Ngừa',
+      descriptions: [
+        'Bảo Trì Cắt Điện, Xử Lý Sự Cố',
+        'Phản Ứng Tình Huống Khẩn Cấp',
+      ],
+    },
+    {
+      link: '/',
+      img: '/assets/images/services/5.JPG',
+      title: 'Bảo Trì Theo Tình Trạng',
+      descriptions: [
+        'Quét Nhiệt Hồng Ngoại',
+        'Đo Phóng Điện Cục Bộ',
+      ],
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
